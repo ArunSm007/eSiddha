7449e84f45bc3544938c784a96e694c0b7fb02b9
ArunSm007 <aaarunmarun7@gmail.com>
2017-03-31 15:36:41 +0530

    del

--- src/main/java/com/esiddha/services/LoginDao.java
+++
@@ -1,9 +0,0 @@
-package com.esiddha.services;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.esiddha.entities.LoginDetails;
-
-public interface LoginDao extends CrudRepository<LoginDetails, String>{
-
-}
